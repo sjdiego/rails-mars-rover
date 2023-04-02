e07237a00317f4f7d0e318b2f916eedae985781b
Diego <diego@diego.cat>
2023-04-02 21:17:10 +0200

    Remove old config host rule

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -69,8 +69,4 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-
-  config.web_console.permissions = '213.94.0.0/16'
-
-  Rails.application.config.hosts << 'gkkoqauc5zraomvr15qrocw7o6xfomw9.diego.cat'
 end
